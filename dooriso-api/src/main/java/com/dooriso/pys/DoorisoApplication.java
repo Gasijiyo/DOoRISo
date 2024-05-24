7b57a3d81cc1abc25d449d1acbea20fd98ba56f6
Gasijiyo <pys9307@naver.com>
2024-05-24 23:16:57 +0900

    .

---
+++ dooriso-api/src/main/java/com/dooriso/pys/DoorisoApplication.java
@@ -0,0 +1,13 @@
+package com.dooriso.pys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorisoApplication.class, args);
+	}
+
+}
---
+++ dooriso-api/src/test/java/com/dooriso/pys/DoorisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooriso.pys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
